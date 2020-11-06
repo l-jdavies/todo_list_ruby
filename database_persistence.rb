6285fbfe55494ae65fd628872236322f4dd61ede
Laura Davies <drljdavies@gmail.com>
2020-11-05 16:04:37 -1000

    Moved logic from application to db

--- database_persistence.rb
+++ database_persistence.rb
@@ -21,15 +21,20 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = "SELECT * FROM lists WHERE id = $1"
+    sql = <<~SQL
+      SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+        WHERE lists.id = $1
+        GROUP BY lists.id
+        ORDER BY lists.name
+      SQL
+
     result = query(sql, id)
 
-    tuple = result.first
-
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-
-    {id: list_id, name: tuple["name"], todos: todos}
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
@@ -46,10 +51,7 @@ class DatabasePersistence
     result = query(sql)
 
     result.map do |tuple|
-      { id: tuple["id"].to_i,
-        name: tuple["name"],
-        todos_count: tuple["todos_count"].to_i,
-        todos_remaining_count: tuple["todos_remaining_count"].to_i }
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -91,8 +93,6 @@ class DatabasePersistence
     query(sql, list_id)
   end
 
-  private
-
   def find_todos_for_list(list_id)
     todo_sql = "SELECT * FROM todos WHERE list_id = $1"
     todo_result = query(todo_sql, list_id)
@@ -102,6 +102,15 @@ class DatabasePersistence
         name: todo_tuple["name"], 
         completed: todo_tuple["completed"] == "t" }
       end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
 
